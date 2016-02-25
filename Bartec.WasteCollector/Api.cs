80fe5799e8e27962b97f9d59ed4211a301334545
Ben <BforBen@users.noreply.github.com>
2016-02-25 20:31:53 +0000

    Initial commit

---
+++ Bartec.WasteCollector/Api.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Bartec.WasteCollector.Auth;
+using Bartec.WasteCollector.Service;
+
+namespace Bartec.WasteCollector
+{
+    public class Api
+    {
+        private static Api _Instance;
+        private string _Token;
+        private DateTime _TokenExpires;
+
+        private Api()
+        {
+            var PrimeToken = Token;
+            Service = new WasteCollectorAPIWebServiceSoapClient("WasteCollectorAPIWebServiceSoap", Properties.Settings.Default.WasteCollectorServiceUri);
+        }
+
+        public static Api Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new Api();
+                }
+                return _Instance;
+            }
+        }
+
+        public string Token
+        {
+            get
+            {
+                // A Bartec token lasts for 60 minutes
+                if (String.IsNullOrWhiteSpace(_Token) || _TokenExpires <= DateTime.Now)
+                {
+                    var AuthService = new AuthenticationAPIWebServiceSoapClient("AuthenticationAPIWebServiceSoap", Properties.Settings.Default.AuthenticationUri);
+                    var Auth = AuthService.Authenticate(Properties.Settings.Default.ServiceUsername, Properties.Settings.Default.ServicePassword);
+
+                    if (Auth.Errors.Error != null)
+                    {
+                        throw new Exception("Unable to authenticate with WasteCollector" + ((String.IsNullOrWhiteSpace(Auth.Errors.Error.Message)) ? "." : " - " + Auth.Errors.Error.Message));
+                    }
+                    else
+                    {
+                        var AuthToken = Auth.Token;
+                        _TokenExpires = DateTime.Now.AddMinutes(59);
+                        _Token = AuthToken.TokenString;
+                    }
+                }
+
+                return _Token;
+            }
+        }
+
+        public WasteCollectorAPIWebServiceSoapClient Service;
+    }
+}
---
+++ Bartec.WasteCollector/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bartec.WasteCollector")]
+[assembly: AssemblyDescription("An interface for the Bartec WasteCollector API.")]
+[assembly: AssemblyCompany("Guildford Borough Council")]
+[assembly: AssemblyProduct("Bartec.WasteCollector")]
+[assembly: AssemblyCopyright("Copyright © Ben Cheetham 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51979458-ae52-48f8-8b63-f235a7956983")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
